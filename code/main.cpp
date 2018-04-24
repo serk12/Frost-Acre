ad557ec380a3b220fd4a93c33036318f7ff27fea
serk <marc.prat.maso@est.fib.upc.edu>
2018-04-24 13:38:09 +0200

    + new point

--- code/main.cpp
+++ code/main.cpp
@@ -9,27 +9,14 @@
 #include "InputBundle/header/ObjManager.h"
 
 int main() {
-    // Model3D model;
-    // model.vertex       = Eigen::MatrixXd(3, 2);
-    // model.vertex(0, 0) = 12.32;
-    // model.vertex(0, 1) = 12.32;
-    // model.vertex(0, 2) = 12.32;
-    // model.vertex(1, 0) = 3.21;
-    // model.vertex(1, 1) = 3.21;
-    // model.vertex(1, 2) = 3.21;
-    // model.edge         = MatrixXB(2, 2);
-    // model.edge(0, 0)   = true;
-    // model.edge(0, 1)   = true;
-    // model.edge(1, 0)   = true;
-    // model.edge(1, 1)   = false;
-    // Instrument *instrument   = new Instrument();
-    // String     *string       = new String(model);
-    // SimulatorManager *simMan = new SimulatorManager();
-    //
-    // Sound sound = simMan->simulateAll(*string, *instrument);
-    //
-    // delete simMan;
-    // delete string;
+    Model3D model = ObjManager::readObj("./test/cube.obj");
 
-    Model3D test = ObjManager::readObj("./test/cube.obj");
+    Instrument *instrument   = new Instrument();
+    String     *string       = new String(model);
+    SimulatorManager *simMan = new SimulatorManager();
+
+    Sound sound = simMan->simulateAll(*string, *instrument);
+
+    delete simMan;
+    delete string;
 }
