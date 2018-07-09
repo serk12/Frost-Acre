18df411d6a74a3a1445222f071fa95e50ad70a14
root <root@DESKTOP-USS1PIH.localdomain>
2018-07-09 17:08:58 +0200

    pickupSimulator

--- code/SimulatorBundle/header/PickupSimulator.h
+++ code/SimulatorBundle/header/PickupSimulator.h
@@ -1,11 +1,21 @@
 #ifndef PICKUPSIMULATOR_HH
 #define PICKUPSIMULATOR_HH
+#define M_PI 3.14159265358979323846
+
+#include <math.h>
 
 class PickupSimulator {
 public:
+    static const double n;
+    double xWire, yWire, zWire, xPickup, yPickup, zPickup, gamma, radiusPickup;
+    double calculate(double xWire, double yWire, double zWire);
 
+    void setGamma(double gamma);
+    void setRadiusPickup(double radiusPickup);
+    void setPickupPossition(double xPickup, double yPickup, double zPickup);
 
 private:
+    double magneticFieldModul(double phi, double rho);
 };
 
 #endif // ifndef PICKUPSIMULATOR_HH
--- code/SimulatorBundle/src/PickupSimulator.cpp
+++ code/SimulatorBundle/src/PickupSimulator.cpp
@@ -1,1 +1,38 @@
 #include "../header/PickupSimulator.h"
+
+const double PickupSimulator::n = 10;
+
+double PickupSimulator::magneticFieldModul(double phi, double rho) {
+    double x = xWire - (xPickup - rho * std::cos(phi));
+    double y = yWire - (yPickup - rho * std::sin(phi));
+    double z = zWire - zPickup;
+    return (gamma * rho) / (x * x + y * y + z * z);
+}
+
+double PickupSimulator::calculate(double xWire, double yWire, double zWire) {
+    this->xWire = xWire; this->yWire = yWire; this->zWire = zWire;
+    double result = 0;
+    for (double phi = 0; phi <= 2 * M_PI; phi += 2 * M_PI / PickupSimulator::n) {
+        for (double rho = 0; rho <= radiusPickup; rho +=  radiusPickup / PickupSimulator::n) {
+            double field = magneticFieldModul(phi, rho);
+            if ((phi != 0) || ((phi +      2 * M_PI / PickupSimulator::n) >     2 * M_PI)) field *= 2;
+            if ((rho != 0) || ((rho +  radiusPickup / PickupSimulator::n) > radiusPickup)) field *= 2;
+            field  *= ((2 * M_PI / PickupSimulator::n) * (radiusPickup / PickupSimulator::n)) / 4;
+            result += field;
+        }
+    }
+    return result;
+}
+
+
+void PickupSimulator::setGamma(double gamma) {
+    this->gamma = gamma;
+}
+void PickupSimulator::setRadiusPickup(double radiusPickup) {
+    this->radiusPickup = radiusPickup;
+}
+void PickupSimulator::setPickupPossition(double xPickup, double yPickup, double zPickup) {
+    this->xPickup = xPickup;
+    this->yPickup = yPickup;
+    this->zPickup = zPickup;
+}
